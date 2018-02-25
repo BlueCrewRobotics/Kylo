6b6345d59a2931bb1f244f248e76867856ec7d7e
Jacob Mealey <jacobmealey.maine@gmail.com>
2018-02-24 22:37:59 -0500

    Offloaded pneumatics code to prevent copy and pasting 8 lines of code

--- components/binaryPneumatic.py
+++ components/binaryPneumatic.py
@@ -5,17 +5,17 @@ class BinaryPneuamtic:
     state = False
     
     def __init__(self, solenoidType, name):
-        pass
-        # self.solenoid = solenoidType
-        # self.mechName = name
+    
+        self.solenoid = solenoidType
+        self.mechName = name
 
     def switch(self):
-        pass
-        # if (self.state == False):
-        #     self.solenoid.set(True)
-        #     self.state = True
-        #     wpilib.SmartDashboard.putString(self.mechName, "Engaged")
-        # elif (self.state == True):
-        #     self.solenoid.set(False)
-        #     self.state = False
-        #     wpilib.SmartDashboard.putString(self.mechName, "Disengaged")
+      
+        if (self.state == False):
+            self.solenoid.set(True)
+            self.state = True
+            wpilib.SmartDashboard.putString(self.mechName, "Engaged")
+        elif (self.state == True):
+            self.solenoid.set(False)
+            self.state = False
+            wpilib.SmartDashboard.putString(self.mechName, "Disengaged")
