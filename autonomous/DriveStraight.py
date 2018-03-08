820b3a096b73b5923353fc622e4a22407f2788fa
Matt Gallant <chillinerd@gmail.com>
2018-03-08 15:05:22 -0500

    Finish Drive Straight Auto

--- autonomous/DriveStraight.py
+++ autonomous/DriveStraight.py
@@ -7,26 +7,34 @@ class Straight(AutonomousStateMachine):
 
     MODE_NAME = 'Drive Straight'
     DEFAULT = True
+
+    def __init__(self):
     
-    self.drivetrain = DriveTrain
-    self.cubemech = CubeMech
-    
-    drive_speed = tunable(0.5)
+        self.drivetrain = DriveTrain
+        self.cubemech = CubeMech        
 
     @timed_state(duration=7, next_state='moveForward', first=True)
     def dontMove(self):
         # Pressurize Pneumatics
         self.cubemech.startPressurize()
 
-    @timed_state(duration=3, next_state='pressurize')
+    @timed_state(duration=3, next_state='stop')
     def moveForward(self):
         # Pressurize Pneumatics
         self.cubemech.startPressurize()
 
         # Drive Forward
         self.drivetrain.arcadeDrive(1.0, 0)
-
-    @timed_state(duration=5)
-    def pressurize(self):
+    
+    @timed_state(duration=2, next_state='moveBackwards')
+    def stop(self):
         # Pressurize Pneumatics
         self.cubemech.startPressurize()
+
+    @timed_state(duration=3)
+    def moveBackwards(self):
+        # Pressurize Pneumatics
+        self.cubemech.startPressurize()
+
+        # Drive Forward
+        self.drivetrain.arcadeDrive(-1.0, 0)
