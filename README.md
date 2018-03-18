b8b37e77633e36236ee41cdce5dcecc553d2d685
Matt Gallant <chillinerd@gmail.com>
2018-03-18 12:54:34 -0400

    Fix Driving System

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Pine Tree:
 - ~~Fix Game Data Detection~~
 - ~~Center Auto Mode~~
 - Accelerometer for Distance
-- Arc/Curved Driving?
+- ~~Arc/Curved Driving~~
 - ~~Increase Driving Speed~~
 - ~~Test Exponential Driving~~
 - ~~Increase Shooter Speed~~
 - ~~Re-enable Second Camera~~
-- Fix Driving (Fix Only One Direction at a Time)
+- ~~Fix Driving (Fix Only One Direction at a Time)~~
 
 New Englands:
 - Implement Vision Tracking
--- common/driveControls.py
+++ common/driveControls.py
@@ -25,30 +25,38 @@ class driveControls (threading.Thread):
         self.ramp = ramp
         self.driveSpeed = 0
         self.turnSpeed = 0
+        self.summedSpeed = 0
 
     def run(self):
 
         while True:
 
             time.sleep(self.delay)
+
+            self.summedSpeed = self.joystick.getRawAxis(2) * -0.8 + self.joystick.getRawAxis(3) * 0.8
             
-            if (self.driveController.y() and self.joystick.getX() > 0):
-                self.turnSpeed = self.joystick.getX() / 1.75
-            elif (self.driveController.y() and self.joystick.getX() < 0):
-                self.turnSpeed = self.joystick.getX() / 1.75
-            elif (self.driveController.y() and self.driveController.left_trigger()):
-                self.driveSpeed = (self.joystick.getRawAxis(2) * -1) / 1.75 
-            elif (self.driveController.y() and self.driveController.right_trigger()):
-                self.driveSpeed = self.joystick.getRawAxis(3) / 1.75
-            elif (self.driveController.left_trigger()):
-                self.driveSpeed = self.joystick.getRawAxis(2) * -0.8
-                self.turnSpeed = 0
-            elif (self.driveController.right_trigger()):
-                self.driveSpeed = self.joystick.getRawAxis(3) * 0.8
-                self.turnSpeed = 0
+            if (self.joystick.getX() > 0.1 or self.joystick.getX() < -0.1 or self.summedSpeed > 0.1 or self.summedSpeed < -0.1):
+                
+                if (self.summedSpeed > 0.1):
+                    self.driveSpeed = (self.summedSpeed) * 0.7 + 0.3
+                elif (self.summedSpeed < -0.1):
+                    self.driveSpeed = (self.summedSpeed) * 0.7 + -0.3
+                else:
+                    self.driveSpeed = 0
+                
+                if (self.joystick.getX() > 0.1):
+                    self.turnSpeed = (self.joystick.getX() * 0.8) * 0.7 + 0.3
+                elif (self.joystick.getX() < -0.1):
+                    self.turnSpeed = (self.joystick.getX() * 0.8) * 0.7 + -0.3
+                else:
+                    self.turnSpeed = 0
+
+                print("Summed Speed: " + str(self.summedSpeed))
+                print("Turn Speed: " + str(self.turnSpeed))
+                
             else:
                 self.driveSpeed = 0
-                self.ramp.stopLeft()
-                self.turnSpeed = self.joystick.getX() * 0.8
+                self.turnSpeed = 0
+                self.summedSpeed = 0
             
             self.drivetrain.arcadeDrive(self.driveSpeed, self.turnSpeed)
